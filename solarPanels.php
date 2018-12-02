4455ec3bd728d97dbda7b62a70ba46b8d95ca936
CharalamposTheodorou <31764602+CharalamposTheodorou@users.noreply.github.com>
2018-12-02 23:21:58 +0200

    POD for solar panel

---
+++ solarPanels.php
@@ -0,0 +1,28 @@
+<?php
+class PV
+{
+  private $conn;
+  private $table_name="solarPanels";
+
+
+  public $id;
+  public $name;
+  public $photo;
+  public $x_cord;
+  public $y_cord;
+  public $address;
+  public $operator_name;
+  public $commision_date;
+  public $description;
+  public $system_power;
+  public $annual_production;
+  public $co2;
+  public $reimbursement;
+  public $panel_modules;
+  public $azimuth;
+  public $inclination_angle;
+  public $communication;
+  public $inverter;
+  public $sensors;
+
+} ?>
