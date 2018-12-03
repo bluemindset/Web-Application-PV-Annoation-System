35fac916f646bb68afa3b1025127baa5c82ad549
CharalamposTheodorou <31764602+CharalamposTheodorou@users.noreply.github.com>
2018-12-03 02:40:54 +0200

    with edit and submit buttons

--- app.js
+++ app.js
@@ -57,6 +57,25 @@ var content = "<div id=\"result\"</div><form  name=\"marker\" id=\"markerForm\" 
               "<br><label>Communication</label>"+"       <input type=\"text\" placeholder=\"Enter Communication:\" name=\"Communication\" size=\"15\" required>"+
               "<br><label>Inverter</label>"+"      <input type=\"text\" placeholder=\"Enter Inverter:\" name=\"Inverter\" size=\"15\" required>"+
               "<br><label>Sensors</label>"+"      <input type=\"text\" placeholder=\"Enter Sensors:\" name=\"Sensors\" size=\"10\" required>"+
+              "<br><input type=\"submit\" value=\"Sumbit\">       <input type=\"button\" value=\"Cancel\"></form>";
+
+var full_content= "<div id=\"result\"</div><form  name=\"marker\" id=\"markerForm\" method=\"post\">"+"<label>Name</label> "+"  <input type=\"text\" placeholder=\"Enter Name:\" name=\"Name\" required>"+
+              "<br><label>X-cord</label> "+"   <input type=\"text\" placeholder=\"X-cord:\" size=\"6\" name=\"X_cord\" required>"+
+              "<br><label>Y-cord</label> "+"   <input type=\"text\" placeholder=\"Y-cord:\" size=\"6\" name=\"Y_cord\" required>"+
+              "<br><label>Address</label> "+"   <input type=\"text\" placeholder=\"Enter Address:\" name=\"Address\" required>"+
+              "<br><label>Operator Name</label>"+"     <input type=\"text\" placeholder=\"Enter Operator name:\" name=\"Operator_Name\" size=\"20\" required>"+
+              "<br><label>Commision Date</label>"+"     <input type=\"date\" name=\"Commision_Date\" required>"+
+              "<br><label>Description</label>"+"        <textarea style=\"resize:none;\" cols=\"40\" rows=\"5\" placeholder=\"Enter a Description:\" name=\"Description\" required></textarea>"+
+              "<br><label>System Power</label>"+"        <input type=\"text\" placeholder=\"Enter System Power:\" name=\"System_Power\" size=\"20\" required>"+
+              "<br><label>Annual Production</label>"+"      <input type=\"text\" placeholder=\"in kWh:\" name=\"Annual_Production\" size=\"10\" required>"+
+              "<br><label>CO2 avoided</label>"+"      <input type=\"text\" placeholder=\"in kWh:\" name=\"CO2\" size=\"10\" required>"+
+              "<br><label>Reimbursement</label>"+"      <input type=\"text\" placeholder=\"in euro:\" name=\"Reimbursement\" size=\"8\" required>"+
+              "<br><label>System Panel Modules</label>"+"     <input type=\"text\" placeholder=\"Enter Panel Modules:\" name=\"Panel_Modules\" size=\"15\" required>"+
+              "<br><label>Azimuth Angle</label>"+"          <input type=\"text\" placeholder=\"Enter azimuth:\" name=\"Azimuth\" size=\"12\" required>"+
+              "<br><label>Inclination Angle</label>"+"       <input type=\"text\" placeholder=\"Enter Inclination Angle:\" name=\"Inclination_Angle\" size=\"20\" required>"+
+              "<br><label>Communication</label>"+"       <input type=\"text\" placeholder=\"Enter Communication:\" name=\"Communication\" size=\"15\" required>"+
+              "<br><label>Inverter</label>"+"      <input type=\"text\" placeholder=\"Enter Inverter:\" name=\"Inverter\" size=\"15\" required>"+
+              "<br><label>Sensors</label>"+"      <input type=\"text\" placeholder=\"Enter Sensors:\" name=\"Sensors\" size=\"10\" required>"+
               "<br><input type=\"submit\" value=\"Edit Form\">       <input type=\"button\" value=\"Cancel\"></form>";
     //  "<b>Portland Cycle Safety Map</b>"+
       //"<br />This map is intended to bring dangerous intersections"+
@@ -70,7 +89,7 @@ var content = "<div id=\"result\"</div><form  name=\"marker\" id=\"markerForm\" 
                     var lng = coord[1].split(')');
                     var popup = L.popup( {maxHeight:300 }).setContent(content);
                     alert("Add Solar Panel Location on map at cordinates: \nLA: " + lat[1] + " & LO: " + lng[0]);
-                    L.marker(e.latlng).addTo(map).bindPopup(popup).openPopup();//.openPopup();
+                    L.marker(e.latlng).addTo(map).bindPopup(popup).openPopup().on('click',MarkerClick);//.openPopup();
 
 
                   // var x= mapContainerPos.left+map.latLngToConatinerPoint(e.getLatLng());
@@ -79,6 +98,13 @@ var content = "<div id=\"result\"</div><form  name=\"marker\" id=\"markerForm\" 
                });
               onetime = 1;
             }
-
+    function MarkerClick(e)
+    {
+      var coord = e.latlng.toString().split(',');
+      var lat = coord[0].split('(');
+      var lng = coord[1].split(')');
+      var popup = L.popup( {maxHeight:300 }).setContent(full_content);
+      L.marker(e.latlng).addTo(map).bindPopup(popup).openPopup();
+    }
 document.getElementById('map').style.cursor = '';
 }
