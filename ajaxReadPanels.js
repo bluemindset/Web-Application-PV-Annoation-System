cf9a29d18899575c6e6e5be911f6550807ad7328
CharalamposTheodorou <31764602+CharalamposTheodorou@users.noreply.github.com>
2018-12-03 15:42:57 +0200

    Add files via upload

--- ajaxReadPanels.js
+++ ajaxReadPanels.js
@@ -6,17 +6,9 @@ $(document).ready(function(){
                     $.ajax({
                         type: 'GET',
                         dataType: 'json',
-                        url: "http://localhost/solarpanels/php/readpanels.php",
-                        success:function(data){ 
-                            var datastr = JSON.stringify(data) 
-                            var allPanels = JSON.parse(datastr);
-                            for (var key in allPanels) {
-                                if (allPanels.hasOwnProperty(key)) {
-                                  var panel  = allPanels[key];
-                                  console.log(panel[0].Name);
-                                }
-                            }
+                        url: "http://localhost/solarpanels/php/test.json",
+                        success:function(data){
+                            $("body").append(JSON.stringify(data));
                         }
-                        
                     });
 });
